06e941090fc407ebaa35144bdd06a1ff8c4e9159
sragvi <nsragvi@gmail.com>
2023-07-21 14:07:23 -0400

    Adding qr

---
+++ src/staticStyleComponents/qrComponent/QrComponent.js
@@ -0,0 +1,25 @@
+
+const QrComponent = () => {
+
+
+    return(
+        <div>
+        <div class="container">
+        <div class="qrContainer">
+          <div class="qrImage">
+            <img src="./images/image-qr-code.png">
+          </div>
+          <div class="qrText">
+            <p><b> Improve your front-end skills by building projects</b></p>
+            <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+          </div>
+        </div>
+      </div>
+      
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+    )
+}
